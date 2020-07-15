6bdd5fd55d1bde3d7594581f90e519846e033bdd
sgalella <santi.galella@gmail.com>
2020-07-15 22:29:03 +0200

    Plot grid, state-value and policy
    Grid file represents rewards at each cell. Simplify code in independent functions.

--- policy_iteration/run.py
+++ policy_iteration/run.py
@@ -1,27 +1,168 @@
 import matplotlib.pyplot as plt
-from gridworld import Gridworld
+import numpy as np
+from utils import draw_arrows
 
 
-GAMMA = 1
+GAMMA = 0.9
 REWARD = -1
 THETA = 0.001
+
+WIDTH = 50
+MARGIN = 2
+
+KEY_TO_ACTION = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+GOALS = [(0, 0), (3, 3)]
+
+MIN_VALUE = -50
+MAX_VALUE = 50
+CMAP_VALUE = plt.cm.jet
+CMAP_VALUE.set_bad("black")
+CMAP_POLICY = plt.cm.gray_r
+CMAP_POLICY.set_bad("black")
+
+
+def load_grid(path):
+    with open(path, 'r') as file:
+        grid = [[int(x) for x in line.strip().split(',')] for line in file]
+    return np.array(grid)
+
+
+def run_policy_evaluation(grid, values, policy):
+    new_values = np.zeros(values.shape)
+    for row in range(new_values.shape[0]):
+        for column in range(new_values.shape[1]):
+            if (row, column) not in GOALS:
+                actions = policy[row][column]
+                p = 1 / len(actions)
+                for action in actions:
+                    action_row, action_column = KEY_TO_ACTION[action]
+                    next_row = row + action_row
+                    next_col = column + action_column
+                    if next_row < 0 or next_col < 0 or next_row >= new_values.shape[0] or next_col >= new_values.shape[1]:
+                        new_values[row][column] += p * (grid[row][column] + GAMMA * values[row][column])
+                    else:
+                        new_values[row][column] += p * (grid[row][column] + GAMMA * values[next_row][next_col])
+    delta = np.max(abs(new_values - values))
+    values = new_values
+    return (values, delta < THETA)
+
+
+def get_max_neighbors(values, row, column):
+    value_neighbors = np.zeros((4, ))
+    for idx, action in enumerate("UDLR"):
+        action_row, action_column = KEY_TO_ACTION[action]
+        next_row = row + action_row
+        next_col = column + action_column
+        if next_row < 0 or next_col < 0 or next_row >= values.shape[0] or next_col >= values.shape[1]:
+            value_neighbors[idx] = -np.inf
+        else:
+            value_neighbors[idx] = values[next_row][next_col]
+    return (value_neighbors == np.nanmax(value_neighbors)).astype(int).tolist()
+
+
+def run_policy_improvement(values):
+    new_policy = np.zeros(values.shape, dtype=object)
+    for row in range(new_policy.shape[0]):
+        for column in range(new_policy.shape[1]):
+            if (row, column) not in GOALS:
+                policy_neighbors = ""
+                max_neighbors = get_max_neighbors(values, row, column)
+                up, down, left, right = max_neighbors
+                if up:
+                    policy_neighbors += "U"
+                if down:
+                    policy_neighbors += "D"
+                if left:
+                    policy_neighbors += "L"
+                if right:
+                    policy_neighbors += "R"
+                new_policy[row][column] = policy_neighbors
+    return new_policy
+
+
+def initialize_initial_values(grid):
+    return np.zeros(grid.shape)
+
+
+def initialize_random_policy(grid):
+    policy = np.zeros(grid.shape, dtype=object)
+    policy[policy == 0] = "UDLR"
+    for (row, column) in GOALS:
+        policy[row][column] = ''
+    return policy
+
+
+def plot_gridworld(ax, grid):
+    ax.imshow(grid, cmap=CMAP_VALUE, vmin=MIN_VALUE, vmax=MAX_VALUE)
+    for row in range(grid.shape[0]):
+        for column in range(grid.shape[1]):
+            ax.text(column, row, f"{grid[row][column]:.1f}", va="center", ha="center", color="black", fontsize=12)
+    ax.set_title("$R$")
+    ax.grid(color="black", linewidth=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.draw()
+
+
+def plot_state_values(ax, values, iteration):
+    ax.imshow(values, cmap=CMAP_VALUE, vmin=MIN_VALUE, vmax=MAX_VALUE)
+    for row in range(values.shape[1]):
+        for column in range(values.shape[1]):
+            ax.text(column, row, f"{values[row][column]:.1f}", va="center", ha="center", color="black", fontsize=12)
+    ax.set_title(f"$V_{{{iteration}}}$")
+    ax.grid(color="black", linewidth=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.draw()
+
+
+def plot_policy(ax, policy, iteration):
+    background = np.zeros(policy.shape)
+    ax.imshow(background, cmap=CMAP_POLICY)
+    for row in range(policy.shape[0]):
+        for column in range(policy.shape[1]):
+            if (row, column) not in GOALS:
+                draw_arrows(ax, policy[row][column], column, row)
+    ax.set_title(r"$\Pi_{" + str(iteration) + "}$")
+    ax.grid(color="black", linewidth=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.draw()
 
 
 def main():
     has_converged = False
-    plt.ion()
-    fig, ax = plt.subplots(1, 2, figsize=(8, 4))
     iteration = 0
-    while not has_converged:
-        env.plot(ax, iteration)
-        has_converged = env.run()
-        iteration += 1
+    state_values = initialize_initial_values(grid)
+    policy = initialize_random_policy(grid)
+    # Initial plot
+    plt.ion()
+    fig, ax = plt.subplots(1, 3, figsize=(8, 4))
+    plot_gridworld(ax[0], grid)
+    plot_state_values(ax[1], state_values, iteration)
+    plot_policy(ax[2], policy, iteration)
+    while True:
+        if not has_converged:
+            state_values, has_converged = run_policy_evaluation(grid, state_values, policy)
+            ax[1].cla()
+            plot_state_values(ax[1], state_values, iteration)
+            plt.pause(0.01)
+        else:
+            new_policy = run_policy_improvement(state_values)
+            if (new_policy == policy).all():
+                break
+            policy = new_policy
+            state_values = initialize_initial_values(grid)
+            has_converged = False
+            ax[2].cla()
+            iteration += 1
+            plot_policy(ax[2], policy, iteration)
+            plt.pause(1)
     plt.ioff()
-    env.plot(ax, iteration)
     plt.show()
 
 
 if __name__ == "__main__":
     filename = "examples/example1.txt"
-    env = Gridworld(filename, GAMMA, REWARD, THETA)
+    grid = load_grid(filename)
     main()
--- policy_iteration/utils.py
+++ policy_iteration/utils.py
@@ -1,75 +1,75 @@
 
-ARROW_LEN = 0.3
+ARROW_LEN = 0.4
 ARROW_COLOR = "black"
 CENTER_OFFSET = 0.05
 
 
-def draw_arrows(max_neighbors, ax, row, column):
+def draw_arrows(ax, policy, row, column):
     """ Draws arrows in cell """
-    if max_neighbors == [1, 0, 0, 0]:
-        draw_arrow_N(ax, row, column)
-    elif max_neighbors == [0, 1, 0, 0]:
-        draw_arrow_S(ax, row, column)
-    elif max_neighbors == [0, 0, 1, 0]:
-        draw_arrow_W(ax, row, column)
-    elif max_neighbors == [0, 0, 0, 1]:
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [1, 1, 0, 0]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_S(ax, row, column)
-    elif max_neighbors == [1, 0, 1, 0]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_W(ax, row, column)
-    elif max_neighbors == [1, 0, 0, 1]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [0, 1, 1, 0]:
-        draw_arrow_S(ax, row, column)
-        draw_arrow_W(ax, row, column)
-    elif max_neighbors == [0, 1, 0, 1]:
-        draw_arrow_S(ax, row, column)
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [0, 0, 1, 1]:
-        draw_arrow_W(ax, row, column)
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [1, 1, 1, 0]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_S(ax, row, column)
-        draw_arrow_W(ax, row, column)
-    elif max_neighbors == [1, 1, 0, 1]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_S(ax, row, column)
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [1, 0, 1, 1]:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_W(ax, row, column)
-        draw_arrow_E(ax, row, column)
-    elif max_neighbors == [0, 1, 1, 1]:
-        draw_arrow_S(ax, row, column)
-        draw_arrow_W(ax, row, column)
-        draw_arrow_E(ax, row, column)
+    if policy == "U":
+        draw_arrow_U(ax, row, column)
+    elif policy == "D":
+        draw_arrow_D(ax, row, column)
+    elif policy == "L":
+        draw_arrow_L(ax, row, column)
+    elif policy == "R":
+        draw_arrow_R(ax, row, column)
+    elif policy == "UD":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_D(ax, row, column)
+    elif policy == "UL":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_L(ax, row, column)
+    elif policy == "UR":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_R(ax, row, column)
+    elif policy == "DL":
+        draw_arrow_D(ax, row, column)
+        draw_arrow_L(ax, row, column)
+    elif policy == "DR":
+        draw_arrow_D(ax, row, column)
+        draw_arrow_R(ax, row, column)
+    elif policy == "LR":
+        draw_arrow_L(ax, row, column)
+        draw_arrow_R(ax, row, column)
+    elif policy == "UDL":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_D(ax, row, column)
+        draw_arrow_L(ax, row, column)
+    elif policy == "UDR":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_D(ax, row, column)
+        draw_arrow_R(ax, row, column)
+    elif policy == "ULR":
+        draw_arrow_U(ax, row, column)
+        draw_arrow_L(ax, row, column)
+        draw_arrow_R(ax, row, column)
+    elif policy == "DLR":
+        draw_arrow_D(ax, row, column)
+        draw_arrow_L(ax, row, column)
+        draw_arrow_R(ax, row, column)
     else:
-        draw_arrow_N(ax, row, column)
-        draw_arrow_S(ax, row, column)
-        draw_arrow_W(ax, row, column)
-        draw_arrow_E(ax, row, column)
+        draw_arrow_U(ax, row, column)
+        draw_arrow_D(ax, row, column)
+        draw_arrow_L(ax, row, column)
+        draw_arrow_R(ax, row, column)
 
 
-def draw_arrow_N(ax, row, column):
+def draw_arrow_U(ax, row, column):
     """ Draws arrow pointing to north """
     ax.annotate("", xy=(row, column - ARROW_LEN), xytext=(row, column + CENTER_OFFSET), arrowprops=dict(arrowstyle="->", color=ARROW_COLOR))
 
 
-def draw_arrow_S(ax, row, column):
+def draw_arrow_D(ax, row, column):
     """ Draws arrow pointing to south """
     ax.annotate("", xy=(row, column + ARROW_LEN), xytext=(row, column - CENTER_OFFSET), arrowprops=dict(arrowstyle="->", color=ARROW_COLOR))
 
 
-def draw_arrow_W(ax, row, column):
+def draw_arrow_L(ax, row, column):
     """ Draws arrow pointing to west """
     ax.annotate("", xy=(row - ARROW_LEN, column), xytext=(row + CENTER_OFFSET, column), arrowprops=dict(arrowstyle="->", color=ARROW_COLOR))
 
 
-def draw_arrow_E(ax, row, column):
+def draw_arrow_R(ax, row, column):
     """ Draws arrow pointing to east """
     ax.annotate("", xy=(row + ARROW_LEN, column), xytext=(row - CENTER_OFFSET, column), arrowprops=dict(arrowstyle="->", color=ARROW_COLOR))
